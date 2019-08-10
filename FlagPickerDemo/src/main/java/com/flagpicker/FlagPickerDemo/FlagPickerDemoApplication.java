dd8e15e10e2471935704a1f8b29ab67b054d6871
manojPenupothu <manojpenupothu@gmail.com>
2019-08-10 19:00:43 +0530

    final commit

--- FlagPickerDemo/src/main/java/com/flagpicker/FlagPickerDemo/FlagPickerDemoApplication.java
+++ FlagPickerDemo/src/main/java/com/flagpicker/FlagPickerDemo/FlagPickerDemoApplication.java
@@ -4,11 +4,19 @@ import java.io.IOException;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class FlagPickerDemoApplication {
+public class FlagPickerDemoApplication extends SpringBootServletInitializer{
 
 	
+	@Override
+ 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+  		return application.sources(FlagPickerDemoApplication.class);
+ 	}
+
+
 	public static void main(String[] args) throws IOException {
 		SpringApplication.run(FlagPickerDemoApplication.class, args);
 	}
